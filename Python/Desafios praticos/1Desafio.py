f8781ec44a30e2e5bc59fff3ea9d2128a087dcbd
Pedro Henrique <pedrin23candido@gmail.com>
2026-09-12 19:27:50 -0300

    Lista 2

---
+++ Python/Desafios praticos/1Desafio.py
@@ -0,0 +1,14 @@
+lista= ["Pedro","Ronne","Carlos","Enzo","Arthur"]
+print(lista)
+adicionar= input("Adicione uma palavra: ")
+lista.append(adicionar)
+print(lista)
+
+remover= input("Retire uma palavra: ")
+lista.remove(remover)
+print(lista)
+
+novovalor = input("Alterar o 3º dado: ")
+lista[2] = novovalor
+print(lista)
+
