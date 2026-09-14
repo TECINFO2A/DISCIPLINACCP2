f1e9164952c6f0ae60f0e2fe2a3543c7e8a2aabe
Gabriel Florentino <gabriel.florentino.com@gmail.com>
2026-09-14 20:18:24 -0300

    atividade de python

---
+++ atividade aula.py/exemplo.py
@@ -0,0 +1,9 @@
+nome = input('digite seu nome: ')
+curso= input('digite seu curso: ')
+p= input('digite sua periodo: ')
+p2= input('escreva oque você espera aprender com python: ')
+
+print('Nome:', nome)
+print('Curso:', curso)
+print('Período:', p)
+print('Expectativas:', p2)
