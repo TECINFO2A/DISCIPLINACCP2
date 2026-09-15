faa5e064a5acaa584eca934972c5b51b44ba2d0c
Samuel Medeiros <samuel898661@gmail.com>
2026-09-15 20:41:36 -0300

    Revert "alocação dos meus dados"

--- Samuel Medeiros/atividades codedex (python basico)/atividade 19.py
+++
@@ -1,7 +0,0 @@
-#nessa atividade se foi criado um codigo que simula a musica 99 bottles of beer, e foi assim que ficou
-
-for i in range(99, 0, -1):
-  print(f'{i} bottles of beer on the wall')
-  print(f'{i} bottles of beer')
-  print('Take one down, pass it around')
-  print(f'{i-1} bottles of beer on the wall')
--- Samuel Medeiros/atividades codedex (python basico)/atividade 7.py
+++
@@ -1,6 +0,0 @@
-#nessa atividade se foi criado um codigo que calcula o IMC de uma pessoa
-
-weight = float(input("Digite o seu peso em KG "))
-height = float(input("Digite a sua altura em M "))
-imc = weight / height ** 2
-print(f"Seu IMC é: {imc:.2f}")
