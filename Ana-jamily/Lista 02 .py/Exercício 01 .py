40c68eede34a315a0f04be9f09470aa0f72e8ae3
Ana Jamily Barbosa de Souza <04205039@sempreuninassau.com.br>
2026-09-15 19:25:49 -0300

    Lista 02

---
+++ Ana-jamily/Lista 02 .py/Exercício 01 .py
@@ -0,0 +1,43 @@
+lista = [0, 1 , 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(lista)
+
+lista.append(6)
+print(lista)
+
+lista.insert(2, 7)
+print(lista)
+
+lista.remove(3)
+print(lista)
+
+lista.append(4)
+print(lista) 
+
+lista.count(4)
+print(lista)
+
+#exercício 2-------------
+
+print(lista[:3])
+print(lista[2:7])
+print(lista[::3])
+print(lista[-3:])
+print(lista[:-4])
+
+#Exercício 3-------
+print(lista[5])
+
+#Exercícoo 4----
+lista[6] = 12
+print(lista)
+
+#Exercício 5----
+lista.reverse()
+print(lista)
+
+#Exercício 6----
+lista.sort()
+print(lista)
+
+
