6d5e15306283698b18d8c47e1370792a30245cb6
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-09-15 10:00:40 -0300

    feat: Desafios Estruturas de dados desafio-03 e desafio-04 feitos + fix: Organização da pasta Gabriel-Porto

--- Gabriel-Porto/Desafios-Praticos-Estruturas-de-Dados/desafio-02.py
+++ Gabriel-Porto/Desafios-Praticos-Estruturas-de-Dados/desafio-02.py
@@ -2,19 +2,25 @@
 
 notas = [7.5, 8.0, 6.5, 9.0, 5.5]
 
+print ("\n")
 notas.sort()
+print ("Os 3 maiores: ")
 print (notas [4])
 print (notas [3])
 print (notas [2])
+print ("\n")
 
+print ("O menor valor: ")
 notas.reverse()
 print (notas [4])
+print ("\n")
 
 soma = (notas [0] + notas [1] + notas [2] + notas [3] + notas [4])
-print(soma)
+print("A soma é igual a:", soma)
 
-media = soma /5
-print(media)
+media = soma / 5
+print("A média é igual a:", media)
 
 quantidade_lista = len(notas)
-print (quantidade_lista)
+print ("A quantidade item na lista é:", quantidade_lista)
+print ("\n")
